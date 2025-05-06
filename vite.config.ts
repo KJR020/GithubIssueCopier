360e482440074ac7d1b5bb25965d0e5990a711bf
KJR020 <johnjiro1114@gmail.com>
2025-05-06 11:26:13 +0900

    🎨 fmt codes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,18 +10,18 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        popup: path.resolve(__dirname, 'src/popup/index.html'),
-        content: path.resolve(__dirname, 'src/content/index.tsx'),
-        background: path.resolve(__dirname, 'src/background/index.ts'),
+        popup: path.resolve(__dirname, "src/popup/index.html"),
+        content: path.resolve(__dirname, "src/content/index.tsx"),
+        background: path.resolve(__dirname, "src/background/index.ts"),
       },
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
       },
     },
   },
